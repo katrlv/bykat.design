0d55b54bcc65b534b221f936775a24c65e73de96
katrlv <51521545+katrlv@users.noreply.github.com>
2019-11-03 00:03:33 -0400

    Update work.js

--- src/pages/work.js
+++ src/pages/work.js
@@ -13,8 +13,7 @@ const WorkTitle = styled("h1")`
 const Work = ({ projects, meta }) => (
     <>
         <Helmet
-            title={`Work | Prist, Gatsby & Prismic Starter`}
-            titleTemplate={`%s | Work | Prist, Gatsby & Prismic Starter`}
+            title={`Work | Katryn Relleve | UX & Digital Design`}
             meta={[
                 {
                     name: `description`,
@@ -22,7 +21,7 @@ const Work = ({ projects, meta }) => (
                 },
                 {
                     property: `og:title`,
-                    content: `Work | Prist, Gatsby & Prismic Starter`,
+                    content: `Work | Katryn Relleve | UX & Digital Design`,
                 },
                 {
                     property: `og:description`,
